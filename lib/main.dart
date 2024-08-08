ecba176472df0167292a54d93c9977cb056c5928
Sachin <sachinkrvis160@gmail.com>
2024-08-08 17:12:44 +0530

    no need to change on the master branch

--- lib/main.dart
+++ lib/main.dart
@@ -8,10 +8,10 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   // This widget is the root of your application.
-
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Counter app',
+      title: 'Flutter Demo',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -29,7 +29,7 @@ class MyApp extends StatelessWidget {
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true
+        useMaterial3: true,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
