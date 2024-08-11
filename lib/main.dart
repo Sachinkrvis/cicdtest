e96a5aad26620cbda05e6a2dd4e5e628fe483d5c
Sachin <sachinkrvis160@gmail.com>
2024-08-12 04:33:44 +0530

    18th trial

--- lib/main.dart
+++ lib/main.dart
@@ -147,7 +147,7 @@ class mypage extends StatelessWidget {
     throw UnimplementedError();
     // trial 16 for protecting the branch.(this suppose to not push directly)
     // trial 17 for protecting the branch.(here the reason why 16th trail push directly to the branch because it came form the admin acc)
-    // this trial 18 suppose to not add in the code because it is going to contain some error
+    // this trial 18 suppose to not add in the code because it is going to contain some error.
 
   }
 }
